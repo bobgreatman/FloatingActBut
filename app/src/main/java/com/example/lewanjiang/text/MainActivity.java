ff475f8b66fad424318e0a3880da1cba5f1f1b9a
Bob <83800572@qq.com>
2017-08-19 21:46:28 +0800

    First commit

---
+++ app/src/main/java/com/example/lewanjiang/text/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.lewanjiang.text;
+
+import android.app.Activity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FloatingActionButton fab=(FloatingActionButton)findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"FAB clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
